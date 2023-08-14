7096746523a53166143b64f8e0f84dcc747d16d5
Aadil malik <adilmalik786687@gmail.com>
2023-08-14 21:58:57 +0530

    stopwatch

--- src/component/Clock.jsx
+++ src/component/Clock.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
-import clock from "../images/clock.png"
 const Clock =()=>{
     const [rotation, setRotation] = useState({ hour: 0, minute: 0, second: 0 });
     // console.log(new Date());
@@ -41,9 +40,9 @@ const Clock =()=>{
         </header>
         <div className="flex justify-center items-center h-[100vh] bg-blue-950 " >
             <div className="h-[30rem] w-[30rem] relative image-class clock-class">
-            <div className="h-[17%] w-[0.5%] left-[49.9%] top-[33%] absolute reshour-class" id="hour" style={{ transform: `rotate(${rotation.hour}deg)` }}></div>
-            <div className="h-[25%] w-[0.5%] left-[49.9%] top-[25%] absolute resminute-class" id="minute"style={{ transform: `rotate(${rotation.minute}deg)` }}></div>
-            <div className="h-[30%] w-[0.5%] left-[49.9%] top-[20%] absolute ressecond-class"  id="second"style={{ transform: `rotate(${rotation.second}deg)` }}></div>
+            <div className="h-[17%] w-[0.5%] left-[49.9%] top-[33%] absolute " id="hour" style={{ transform: `rotate(${rotation.hour}deg)` }}></div>
+            <div className="h-[25%] w-[0.5%] left-[49.9%] top-[25%] absolute " id="minute"style={{ transform: `rotate(${rotation.minute}deg)` }}></div>
+            <div className="h-[30%] w-[0.5%] left-[49.9%] top-[20%] absolute "  id="second"style={{ transform: `rotate(${rotation.second}deg)` }}></div>
             </div>
         </div>
         </>
--- src/component/Stopwatch.jsx
+++ src/component/Stopwatch.jsx
@@ -1,11 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState,useEffect } from "react";
 const Stopwatch =()=>{
+    const [second, setSecond] = useState(0);
+        let [minute, setMinute] = useState(0);
+        let [hour, setHour] = useState(0);
+        const [stop, setStop] = useState(false)
+        const [id, setId] = useState(false);
+        //---------------------------------------------------------------------- 
+        useEffect(() => {
+            if (stop) {
+                setId(setInterval(() => {
+                    setSecond((second)=>second+1)
+                }, 1000))   
+            }
+            else{
+                clearInterval(id)
+            }
+        },[stop]);
+        //--------------------------------------------------------------------
+
+        useEffect(() => {
+            if (second >= 60) {
+                setMinute(minute + 1);
+                setSecond(0);
+            }
+        }, [second]);
+
+        //---------------------------------------------------------------------------
+
+        useEffect(() => {
+            if (minute == 59 && second==60) {
+
+                setSecond(0);
+                setMinute(0);
+                setHour(hour + 1);
+            }
+        }, [minute,second]);
+
+        //----------------------------------------------------------------------
+        //events--------------------------------------------------------------
+        const startEventHandler = () => {
+        setStop(true);
+        }
+        const stopEventHandler=()=>{
+            setStop(false);
+        }
+
+        const resetEventHandler=()=>{
+            setSecond(0);
+            setMinute(0);
+            setHour(0);
+            setStop(false);
+        }
     return(
         <>
-        <header className="bg-slate-800 h-20 font-serif">
+        <header className="bg-slate-800 font-serif">
             <nav>
-                <ul className="flex justify-evenly items-center h-20 text-4xl text-sky-700">
+                <ul className="flex justify-evenly items-center h-20 text-4xl text-sky-700 resnavi-class">
                 <li>
                     <Link to="/">WorldClock</Link>
                     </li>
@@ -24,7 +76,39 @@ const Stopwatch =()=>{
                 </ul>
             </nav>
         </header>
-        <h1>Stopwatch</h1>
+        <section className="bg-blue-500 h-[100vh] flex flex-col gap-[50px] justify-center items-center ">
+                        <article className="flex text-[200px] justify-center items-center parent-class ">
+                            
+                            {hour<10?<div className="bg-blue-400 h-[200px] flex justify-center items-center rounded-[20px] reshour-class  ">
+                                {"0"+hour}
+                            </div>:<div className="bg-blue-400 h-[200px] flex justify-center items-center rounded-[20px] reshour-class">
+                                {hour}
+                            </div>}
+                            
+                            <span>:</span>
+                            {minute<10?<div className="bg-blue-400 h-[200px] flex justify-center items-center rounded-[20px] resmin-class">
+                                {"0"+minute}
+                            </div>:
+                            <div className="bg-blue-400 h-[200px] flex justify-center items-center rounded-[20px] resmin-class">
+                                {minute}
+                            </div>}
+                                <span className="">:</span>
+                        {second<10?(
+                            <div className="bg-blue-400 h-[200px] flex justify-center items-center rounded-[20px] ressec-class">
+                                {"0"+second}
+                            </div>
+                            ):<div className="bg-blue-400 h-[200px] flex justify-center items-center rounded-[20px] ressec-class">
+                            {second}
+                        </div>}
+                        </article>
+                <section className="text-[50px] w-[100%] flex justify-center gap-[50px] bg-blue-500 superparent-class" >
+                    <button className="border-[2px] border-black rounded-[20px] text-[25px] h-[70px] w-[150px] hover:bg-blue-400 resstart-class" onClick={startEventHandler} >Start
+                    </button>
+                    <button className="border-[2px] border-black rounded-[20px] text-[25px] h-[70px] w-[150px] hover:bg-blue-400 resstop-class" onClick={stopEventHandler}>Stop</button>
+                    <button className="border-[2px] border-black rounded-[20px] text-[25px] h-[70px] w-[150px] hover:bg-blue-400 resreset-class" onClick={resetEventHandler}>Reset</button>
+                </section>
+                </section>
+        
         </>
     )
 }
