18657018d323b67eefa0f4f8bf9a7da69d4c44c3
Aadil malik <adilmalik786687@gmail.com>
2023-08-17 22:16:40 +0530

    alarm

--- src/component/Alarm.jsx
+++ src/component/Alarm.jsx
@@ -1,11 +1,137 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 // import { Link } from "react-router-dom";
+import music from "../audio/music.mp3"
 import Header from "../component/Header";
-const Alarm =()=>{
-    return(
+const Alarm = () => {
+    const audioref=useRef();
+    const [timehour, setTimeHour] = useState(new Date().toTimeString().split(" ")[0].split(":")[0]);
+    const [timeminute, setTimeMinute] = useState(new Date().toTimeString().split(" ")[0].split(":")[1]);
+    const [timesecond, setTimeSecond] = useState(new Date().toTimeString().split(" ")[0].split(":")[2]);
+    const [meridiem, setMeridiem] = useState(new Date().toLocaleTimeString().split(" ")[1]);
+    const [realtime, setRealTime] = useState();
+    // console.log(new Date().toTimeString().split(" ")[0].split(":")[0]);
+    // console.log(new Date().toLocaleTimeString().split(" ")[1]);
+    const [hour, setHour] = useState("");
+    const [minute, setMinute] = useState("");
+    const [second, setSecond] = useState("");
+    const [aftermeridiem, setAfterMeridiem] = useState("");
+    const [alarms,setAlarms]=useState([]);
+    const [isAlarmPlaying, setIsAlarmPlaying] = useState(false);
+
+    function audio(){
+        
+        if(audioref.current){
+            audioref.current.load();
+            audioref.current.play();
+            setIsAlarmPlaying(true);
+        }
+    }
+    function stop(){
+        if(audioref.current){
+        audioref.current.pause();
+        setIsAlarmPlaying(false);
+    }
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // console.log("Checking alarm conditions...");
+            setTimeHour(new Date().toTimeString().split(" ")[0].split(":")[0]);
+            setTimeMinute(new Date().toTimeString().split(" ")[0].split(":")[1]);
+            setTimeSecond(new Date().toTimeString().split(" ")[0].split(":")[2]);
+            setMeridiem(new Date().toLocaleTimeString().split(" ")[1]);
+            // console.log(new Date().toTimeString().split(" ")[0].split(":")[2]);
+            alarms.forEach(element => {
+                
+                if (timehour === element.hour && timeminute === element.minute && timesecond === element.second && meridiem === element.aftermeridiem) {
+                    audio();
+                }
+            });
+        },1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }); // Include both time and alarm in the dependency array.
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRealTime(new Date().toTimeString().split(" ")[0])
+        }, 1000)
+        return () => {
+            clearInterval(interval);
+        };
+    },[realtime])
+
+    const handleChangeHour = (e) => {
+        setHour(e.target.value);
+        // console.log(e);
+    };
+    const handleChangeMinute = (e) => {
+        setMinute(e.target.value);
+        // console.log(e);
+    };
+    const handleChangeSecond = (e) => {
+        setSecond(e.target.value);
+        // console.log(e);
+    };
+    const handleChangeMeridiem = (e) => {
+        setAfterMeridiem(e.target.value);
+        // console.log(e);
+    };
+    const addAlarm = () => {
+        if (hour !== "" && minute !== "" && second !== ""){
+        const newAlarm = {
+            hour: hour,
+            minute: minute,
+            second: second,
+            aftermeridiem: aftermeridiem
+        };
+
+        setAlarms(prevAlarms => [...prevAlarms, newAlarm]);
+        setHour("");
+        setMinute("");
+        setSecond("");
+        setAfterMeridiem("PM");
+        }
+    };
+    const deleteAlarm = (index) => {
+        stop();
+        setAlarms(prevAlarms => {
+            const newAlarms = [...prevAlarms];
+        newAlarms.splice(index, 1);
+        return newAlarms;
+    });
+    };
+
+    return (
         <>
-       <Header/>
-        <h1>Alarm</h1>
+            <Header />
+            <div>
+            {alarms.map((alarm, index) => (
+                    <div key={index}>{alarm.hour}:{alarm.minute}:{alarm.second} {alarm.aftermeridiem}
+                     <button onClick={() => deleteAlarm(index)}>Delete</button></div>
+                    
+                ))}
+            </div>
+            <div>
+                {/* {timehour}:{timeminute}:{timesecond} {meridiem} */}
+                {realtime} {meridiem}
+            </div>
+            <input type="text" value={hour} onChange={handleChangeHour} className="border" />
+            <input type="text" value={minute} onChange={handleChangeMinute} className="border" />
+            <input type="text" value={second} onChange={handleChangeSecond} className="border" />
+            <input type="text" list="bind" value={aftermeridiem} onChange={handleChangeMeridiem} className="border" />
+           
+            <datalist id="bind">
+                <option value="PM" />
+                <option value="AM" />
+            </datalist>
+            {isAlarmPlaying?<button onClick={stop}>stop alarm</button>:""}
+            
+            <button onClick={addAlarm}>Add Alarm</button>
+            <audio ref={audioref} >
+                <source src={music} />
+            </audio>
         </>
     )
 }
--- src/component/WorldClock.jsx
+++ src/component/WorldClock.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 // import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Header from "../component/Header";
 const WorldClock = () => {
   // console.log(new Intl.DateTimeFormat());
   // const [timeZones, setTimeZones] = useState([]);
+  
   const [currentTime, setCurrentTime] = useState("");
   const [selectedTimeZone, setSelectedTimeZone] = useState('Asia/Kolkata'); // Default time zone
   const [inputValue, setInputValue] = useState('');
@@ -365,7 +366,7 @@ const WorldClock = () => {
       timeZone: selectedTimeZone,
       hour: 'numeric',
       minute: 'numeric',
-      second: 'numeric',
+      second: 'numeric',  
       hour12: true,
     });
 
@@ -416,6 +417,7 @@ const WorldClock = () => {
         <h2 className="text-[40px] text-white currenttime-class">{selectedTimeZone}</h2>
         <p className="text-[100px] my-[-50px] exacttime-class">{currentTime}</p>
       </div>
+     
 
 
     </>
