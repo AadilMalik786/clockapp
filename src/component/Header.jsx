8adf59cc6adf08933d150b0780782dbf55c72363
Aadil malik <adilmalik786687@gmail.com>
2023-08-18 17:26:55 +0530

    hover

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,21 +5,21 @@ const Header=()=>{
         <>
          <header className="bg-slate-800 font-serif">
         <nav>
-          <ul className="flex justify-evenly items-center  h-20 text-4xl text-sky-700 resnavi-class">
-            <li>
-              <Link to="/">WorldClock</Link>
+          <ul className="flex justify-evenly items-center  h-20 text-4xl    text-sky-700 resnavi-class">
+            <li >
+              <Link  to="/" className="hover:text-sky-300 transition duration-700 ease-in " >WorldClock</Link>
             </li>
             <li>
-              <Link to="/Alarm">Alarm</Link>
+              <Link to="/Alarm" className="hover:text-sky-300 transition duration-700 ease-in ">Alarm</Link>
             </li>
             <li>
-              <Link to="/Timer">Timer</Link>
+              <Link to="/Timer" className="hover:text-sky-300 transition duration-700 ease-in ">Timer</Link>
             </li>
             <li>
-              <Link to="/Stopwatch">Stopwatch</Link>
+              <Link to="/Stopwatch" className="hover:text-sky-300 transition duration-700 ease-in ">Stopwatch</Link>
             </li>
             <li>
-              <Link to="/Clock">Clock</Link>
+              <Link to="/Clock" className="hover:text-sky-300 transition duration-700 ease-in ">Clock</Link>
             </li>
           </ul>
         </nav>
